9912a773ed17b7e58729589e43443ad0c7287c54
Mo <mo@iptv.com>
2026-09-03 22:04:10 +0000

    Update server URL to line.truthdaily.me

--- worker/worker.js
+++ worker/worker.js
@@ -7,7 +7,7 @@
 
 const API_BASE    = "https://activationpanel.ru/api/api.php";
 const API_KEY     = "35cf68cc83a3a82e1a0ac5361c7b6105";
-const HOST        = "http://mag.trexlive.me";
+const HOST        = "http://line.truthdaily.me";
 const FROM_EMAIL  = "IPTV Mojo <support@iptvmojo.com>";
 const ADMIN_EMAIL = "support@iptvmojo.com";
 const SITE_URL    = "https://iptvmojo.com";
